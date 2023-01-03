5d10a44770528ef8b8c2177edcae146e5c6b5ef7
Gianluca Mazzeo <g.mazzeo@theinnovationfactory.it>
2023-01-03 11:32:13 +0200

    [config] connectionMongoDB

--- movies-graphql-project-lesson/server/app.js
+++ movies-graphql-project-lesson/server/app.js
@@ -3,22 +3,26 @@ const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
 app.use(cors())
 
-CONNECTION_URL = "mongodb+srv://sefa:test123123@cluster0.sfv3f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+CONNECTION_URL = process.env.MONGODB_CONNECT;
 
 mongoose.connect(CONNECTION_URL)
 mongoose.connection.once('open', () => {
    console.log("Connection to database has been established successfully");
 })
 
+
 app.use('/graphql', graphqlHTTP({
    schema,
    graphiql: true
 }));
+
 
 app.listen(5000, () => {
    console.log('Server running on port 5000');
